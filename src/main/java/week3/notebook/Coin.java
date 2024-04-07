6c0307a7737cc2c7ac8c3ed91335c417487040f6
yuhuan <597270222@qq.com>
2024-04-07 21:54:08 +0800

    week3-对象容器

--- src/main/java/week3/notebook/Coin.java
+++ src/main/java/week3/notebook/Coin.java
@@ -2,7 +2,12 @@ package week3.notebook;
 
 import java.util.HashMap;
 import java.util.Scanner;
-//HashTable
+//散列表（HashMap）
+//散列表是一种数据结构，数据以键值对的形式存储，通过键来获取对应的值。
+//散列表的键必须是对象类型，而不能是基本类型，（容器中元素必须是对象，不能是基本类型（基本类型可以通过包装类转为对象））。
+//散列表中，键是唯一的，如果多次放入相同的键，只会保留最后一次放入的值。
+//通过containsKey方法判断散列表中是否存在某个key。（如果某个key不存在，直接.get(key)会返回null）
+//通过keySet方法获取散列表中的所有键的集合，并通过遍历键的集合来获取对应的value。
 public class Coin {
     private HashMap<Integer,String> coinnames= new HashMap<Integer,String>();
     public Coin() {
